33dc25cb80220e008051ab75ab15eae36456c512
david collett <davida.collett95@gmail.com>
2019-07-11 17:17:38 -0600

    not done yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import './App.css';
 
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
+import Post from './Post/Post'
 
 class App extends Component {
   constructor() {
@@ -19,15 +21,34 @@ class App extends Component {
   }
   
   componentDidMount() {
-
+    axios
+    .get('https://practiceapi.devmountain.com/api/posts').then( (results) => {
+      // console.log(results.data)
+      this.setState({
+        posts: results.data
+      })
+    })
   }
 
-  updatePost() {
+  updatePost(id, text) {
+
+    axios
+    .put(`https://practiceapi.devmountain.com/api/posts?id=${id}`, {text})
+    .then((results) => {
+      this.setState({
+        posts: results.data
+      })
+    })
   
   }
 
-  deletePost() {
-
+  deletePost( id ) {
+    axios.delete(`https://practiceapi.devmountain.com/api/delete/id=${id}`)
+    .then(results => {
+      this.setState({
+        posts: results.data
+      })
+    })
   }
 
   createPost() {
@@ -45,6 +66,19 @@ class App extends Component {
 
           <Compose />
           
+            {
+              posts.map( (post) => (
+                
+                <Post 
+                key={ post.id } 
+                text={ post.text } 
+                date={ post.date }
+                id={ post.id }
+                updatePostFn={ this.updatePost } 
+                deletePostFn={ this.deletePost } />
+              ))
+            }
+
         </section>
       </div>
     );
